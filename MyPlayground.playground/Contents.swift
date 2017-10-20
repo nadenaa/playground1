fc2ba25a048afa73422a2277f28e8f6ea35f6de1
nadenaa <32922893+nadenaa@users.noreply.github.com>
2017-10-20 14:52:08 +0700

    add new code

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,3 +7,36 @@ let nilai = 10
 var name = "hilmy"
 var namalengkap = "hilmy fauzi"
 var fullname = name + namalengkap
+
+let count = 10
+var price = 23.55
+
+let firstMessage = "swift is awasome"
+let secondMessage = "what do you think?"
+var message = firstMessage + secondMessage
+
+print(message)
+
+var message1 = "hello swift how can i get started?"
+var message2 = "the best way to get started is to stop talking and code"
+
+message1.uppercased()
+message2.lowercased() + "okay, i'm working on it "
+//seleksi kondisi
+if message1 == message2 {
+      print("same message")
+} else {
+      print("not the same message")
+}
+
+let mesageLabel = UILabel(frame:CGRect(x: 0, y: 0, width: 300, height: 50))
+mesageLabel.text = message2
+
+mesageLabel
+//mengganti background label
+mesageLabel.backgroundColor = UIColor.orange
+//teks d tengah
+mesageLabel.textAlignment = NSTextAlignment.center
+mesageLabel.layer.cornerRadius = 10.0
+mesageLabel.clipsToBounds = true
+mesageLabel
