fa1d82a7ad8f1c9d5c4f53901dc0500d4b6b35b7
nadenaa <32922893+nadenaa@users.noreply.github.com>
2017-10-20 14:16:46 +0700

    add code

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let nilai = 10
+var name = "hilmy"
+var namalengkap = "hilmy fauzi"
+var fullname = name + namalengkap
